8ed42ba7eb46c721139b6591c3e44429a97be246
Somesh Gusain <someshgusain.0007@gmail.com>
2023-08-30 17:34:06 +0530

    Add existing project files to Git

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'todo';
+export class AppComponent implements OnInit{
+  
+   todoForm = new FormGroup({
+    newTodo: new FormControl('',Validators.required),
+  })
+  todos:any [] = [];
+  constructor() { }
+  ngOnInit() {
+    
+  }
+
+  addTodo() {
+    if (this.todoForm.invalid){
+
+    }
+    else{
+      const val = this.todoForm.getRawValue()
+      const title = val.newTodo
+      this.todos.push((title));
+      
+      this.todoForm.reset({newTodo:''});
+      console.log(this.todoForm);
+      
+    }
+   
+  }
+  removeTodo(todo: string, index:number) {
+    console.log(todo);
+    
+    this.todos = this.todos.filter((todo, i) => i !== index);
+  }
+  clear(){
+    this.todoForm.reset({newTodo:''});
+  }
 }
